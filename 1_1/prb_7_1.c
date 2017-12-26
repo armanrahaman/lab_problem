fccf7869a0b9d754f61896585143ca21bbd5d309
Nafiul Rony <nafiul.ice.pust@gmail.com>
2017-12-26 13:01:16 +0600

    added new solve

---
+++ 1_1/prb_7_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,result=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        result+=i;
+    }
+    printf("result is %d",result);
+    return 0;
+}
